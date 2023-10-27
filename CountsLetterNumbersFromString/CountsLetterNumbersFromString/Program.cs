b97067ab883a5a80d202d5573992d379276199e8
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-27 16:08:01 +0300

    anagram string added

---
+++ CountsLetterNumbersFromString/CountsLetterNumbersFromString/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("bir metin giriniz");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("bir metin giriniz");
+        string text2 = Console.ReadLine();
+
+
+
+        char[] dizi1 = text.ToCharArray();
+        char[] dizi2 = text2.ToCharArray();
+
+
+        Array.Sort(dizi1);
+        Array.Sort(dizi2);
+
+
+
+        bool dogrumu = true;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (dizi1[i] != dizi2[i])
+            {
+                dogrumu = false;
+            }
+
+
+
+        }
+        if (dogrumu == true)
+        {
+            Console.WriteLine("Metinler Anagramdır");
+
+        }
+        else
+        {
+            Console.WriteLine("Metinler Anagram değildir");
+
+        }
+    }
+}
