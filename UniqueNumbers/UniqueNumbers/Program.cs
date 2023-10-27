056d2815237c82b7fa80524be5077615075d3e22
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-27 16:17:32 +0300

    unique added

---
+++ UniqueNumbers/UniqueNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bir metin giriniz.");
+        string text = Console.ReadLine().ToLower();
+
+        bool isUnique = true;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            for (int j = i + 1; j < text.Length; j++)
+            {
+                if (text[i] == text[j])
+                {
+                    Console.WriteLine("notUnique");
+                    return;
+                }
+
+            }
+        }
+
+        Console.WriteLine("Unique");
+    }
+}
