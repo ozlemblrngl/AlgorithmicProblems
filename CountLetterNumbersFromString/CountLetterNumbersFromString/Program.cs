acc6b64d77d553d09f3429a0010dd7734b665325
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-27 16:13:34 +0300

    added

---
+++ CountLetterNumbersFromString/CountLetterNumbersFromString/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //tobeto
+        Console.WriteLine("Bir yazı giriniz.");
+        string text = Console.ReadLine();
+
+        var harfler = text.GroupBy(group => group);
+
+        foreach (var group in harfler)
+        {
+            Console.WriteLine(group.Count() + " adet " + group.Key);
+
+        }
+
+        Console.ReadLine();
+    }
+}
