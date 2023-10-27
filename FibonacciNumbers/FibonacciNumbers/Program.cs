de0445903f65273b41ea569e7e2dfadf2dd59929
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-27 16:02:32 +0300

    fibonacci numbers added

---
+++ FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal number1 = 0;
+        decimal number2 = 1;
+        decimal number3;
+
+
+        for (ulong i = 1; i < 100; i++)
+        {
+
+            number3 = number1 + number2;
+            number1 = number2;
+            number2 = number3;
+
+            Console.WriteLine(i + 1 + ". sayı " + number3 + "-");
+        }
+
+
+
+        // 0,1,1,2,3,5,8,13,21
+
+    }
+}
